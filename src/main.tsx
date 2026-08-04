263351a53c05b1b3ed133c221bdbf7a47b573b26
StevanKlaas <70848201+StevanKlaas@users.noreply.github.com>
2026-08-04 15:16:51 +0200

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import PhotonSim from "./PhotonSim.jsx";
+import PhotonSim from "./PhotonSim";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
